d76d781a8f42d4a4ad1ba5280268aa3454d39ca0
Samuel Xavier <sancrisxa@gmail.com>
2019-05-01 15:53:09 -0300

    finalizacao do projeto

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeriesFormRequest;
 use App\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index() {
-        $series = Serie::all();
+    public function index(Request $request) {
+        $series = Serie::query()
+            ->orderBy('nome')
+            ->get();
+        $mensagem = $request->session()->get('mensagem');
 
-        return view('series.index', compact('series'));
+        return view('series.index', compact('series', 'mensagem'));
     }
 
     public function create()
@@ -18,10 +22,26 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
         $serie = Serie::create($request->all());
+        $request->session()
+            ->flash(
+                'mensagem',
+                "Série {$serie->id} criada com sucesso {$serie->nome}"
+            );
 
-        echo "Série com id {$serie->id} criada: {$serie->nome}";
+        return redirect()->route('listar_series');
+    }
+
+    public function destroy(Request $request)
+    {
+        Serie::destroy($request->id);
+        $request->session()
+            ->flash(
+                'mensagem',
+                "Série removida com sucesso"
+            );
+        return redirect()->route('listar_series');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/series', 'SeriesController@index');
-Route::get('/series/criar', 'SeriesController@create');
+Route::get('/series', 'SeriesController@index')
+    ->name('listar_series');
+Route::get('/series/criar', 'SeriesController@create')
+    ->name('form_criar_serie');
 Route::post('/series/criar', 'SeriesController@store');
+Route::delete('/series/{id}', 'SeriesController@destroy');
